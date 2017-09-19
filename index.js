93fd57bcd6169367456021cd768c332dad34900f
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-19 22:38:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function sayHiToGrandma(string){
   if (string ==="I love you, Grandma."){
     return "I love you, too."
   }else if (string.toLowerCase === string){
-    return "I can't hear you!"
+    return "Bleh"
   }else if (string.toUpperCase === string) {
     return "YES INDEED!"
   }else {
