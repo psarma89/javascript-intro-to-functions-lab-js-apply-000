2d4658a38dbbf7e82614b498993bf710e98ddc16
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-19 21:51:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-// This shout(string) function receives one argument and return it in all caps
+// This shout(string) function receives one argument and returns it in all caps
 function shout(string){
+  return string.toUpperCase()
+}
+
+// This whisper(string) function receives one argument and returns it in all lowercase
+function whisper(string){
   return string.toUpperCase()
 }
