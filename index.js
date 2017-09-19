519b0352b7b7be31d476f6b151296b19880a105d
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-19 22:18:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,5 +30,11 @@ function logWhisper(string){
 
 // This sayHiToGrandma(string) function returns conditional based outputs based on input
 function sayHiToGrandma(string){
-
+  if (string.toLowerCase === string){
+    return "I can't hear you!"
+  }else if (string.toUpperCase === string) {
+    return "YES INDEED!"
+  }else if (string ==="I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
