87b4fc31ffbe3c985fb1f3516da90ef826080797
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-19 22:05:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // This shout(string) function receives one argument and returns it in all caps
 function shout(string){
-  if (string){
-    
+  if (typeof string === 'string'){
+    return string.toUpperCase()
   } else {
-    return string.toString().toUpperCase()
+    console.log("Please Enter an argument of type string ")
   }
 }
 
@@ -18,12 +18,12 @@ function logShout(string){
 
 //This logWhisper(string) function calls console.log() its one argument in all lowercase
 function logWhisper(string){
-  
+
 }
 
 // This sayHiToGrandma(string) function returns conditional based outputs based on input
 function sayHiToGrandma(string){
-  
+
 }
 
 //
