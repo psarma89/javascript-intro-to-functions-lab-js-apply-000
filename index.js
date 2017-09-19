a642d0e5623158dc3a12935a8276f16d5a811adc
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-19 23:05:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function sayHiToGrandma(string){
   }else if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }else if (string === "I love you, Grandma.") {
-    
+    return
   }
 }
 
